14512d92e7de07f96963645572d9ee5b0c5f1fe2
glali1978 <gerelali@gmail.com>
2025-04-11 11:41:55 +0200

    Add files via upload

---
+++ 28_Kolcsonzes_feladat.py
@@ -0,0 +1,31 @@
+"""
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+<<<                                                        >>>
+<<< Könyvkölcsönzési napló (Python gyakorlófeladat)        >>>
+<<<                                                        >>>
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+Feladatleírás
+Készíts egy programot, amely egy könyvtár napi könyvkölcsönzéseit rögzíti, majd különböző információkat szolgáltat az adatokról!
+
+Feladatok
+a) A program kérje be, hány könyvet kölcsönöztek ki az adott napon.
+   (NEM KÖTELEZŐ: while ciklus, hogy csak érvényes, pozitív egész számot fogadjon el!)
+
+b) Minden könyvhöz kérje be:
+   - A könyv címe
+   - A kölcsönző neve
+   - A kölcsönzés időtartama napokban (1–60 nap között)
+
+c) Minden adat megadása után írja ki az eddigi kölcsönzési listát.
+
+d) A program a végén írja ki:
+   - A legtöbb napra kikölcsönzött könyv adatait
+   - A legrövidebb kölcsönzés adatait
+   - Az átlagos kölcsönzési időt (két tizedesre kerekítve)
+   - A könyvek címeit ábécérendben
+
+Extra feladat (nem kötelező):
+- Hibakezelés: Ha hibás adatot adnak meg (pl. szám helyett szöveget vagy túl nagy napértéket), a program ne omoljon össze.
+- Ne engedjük 1 napnál rövidebb vagy 60 napnál hosszabb kölcsönzést.
+"""
