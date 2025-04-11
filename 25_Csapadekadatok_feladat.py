16e8656573a9e54ca082d4b27c7f9e597670cc67
glali1978 <gerelali@gmail.com>
2025-04-11 07:21:40 +0200

    Add files via upload

---
+++ 25_Csapadekadatok_feladat.py
@@ -0,0 +1,27 @@
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# <<<                                                       >>>
+# <<< 25_Csapadékadatok_feladat (Python gyakorlófeladat)    >>>
+# <<<                                                       >>>
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+"""
+Feladatleírás
+Készíts programot, amely egy hét napi csapadékadatát (mm-ben) kéri be, majd elemezi azokat!
+
+Feladatok
+a) Kérdezd meg, hány nap csapadékadatait szeretné a felhasználó rögzíteni!
+
+b) Egy for ciklussal kérd be az egyes napok csapadékértékeit (milliméterben), és tárold el egy listában.
+
+c) Minden új adat után jelenítsd meg az eddigi értékeket!
+
+d) A program a végén írja ki:
+
+Mennyi volt a legtöbb és legkevesebb csapadék (mm-ben)?
+
+Mennyi volt az átlagos csapadék mennyiség (két tizedesjegyre kerekítve)?
+
+A csapadékadatokat növekvő sorrendben.
+"""
+
+
+
---
+++ 27_Szorzotabla_feladat.py
@@ -0,0 +1,24 @@
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# <<<                                                       >>>
+# <<< 27_Szorzotabla_feladat (Python gyakorlófeladat)       >>>
+# <<<                                                       >>>
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+"""
+Feladatleírás:
+Készíts egy kis matek gyakorló programot, ahol a számítógép véletlenszerűen feltesz szorzótábla kérdéseket, és a játékosnak meg kell válaszolnia azokat!
+
+Feladatok:
+a) Kérdezd meg a játékostól, hány kérdést szeretne kapni.
+(Használj while ciklust, hogy csak pozitív egész számot fogadj el!)
+
+b) A program minden körben generáljon egy két szám szorzására vonatkozó kérdést 
+- például:
+„Mennyi 7 x 8?”
+
+c) A felhasználó válaszol, a program pedig visszajelzést ad:
+
+Helyes vagy Helytelen, és megmutatja a jó választ.
+
+d) A végén írja ki, hány helyes választ adott a játékos, és mennyi volt az összpontszáma.
+"""
+
